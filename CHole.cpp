9983b6e63b617da1954c0cbcef117c5e41604dbf
tjdgus3537 <tjdgus3537@naver.com>
2015-11-08 03:16:02 +0900

    구멍 크기 증가 + 공 최초 배치

--- CHole.cpp
+++ CHole.cpp
@@ -28,7 +28,7 @@ bool CHole::create(IDirect3DDevice9* pDevice, D3DXCOLOR color)
 	m_mtrl.Emissive = d3d::BLACK;
 	m_mtrl.Power = 5.0f;
 
-	if (FAILED(D3DXCreateSphere(pDevice, getRadius(), 50, 50, &m_pSphereMesh, NULL)))
+	if (FAILED(D3DXCreateSphere(pDevice, 0.28f, 50, 50, &m_pSphereMesh, NULL)))
 	{
 		return false;
 	}
