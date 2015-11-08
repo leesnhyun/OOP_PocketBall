ccf52f1c6c50851c046872be7ff77893c50bd056
Donghwan Kim <wraithtorch@gmail.com>
2015-11-08 20:30:33 +0900

    sry2

--- FoulManager.h
+++ FoulManager.h
@@ -6,6 +6,10 @@ class FoulManager
 	bool foul;
 	bool lose;
 public:
+	FoulManager();
+
+	void reset();
+
 	// 규칙을 위반한 경우 true 반환
 	// 파울
 	bool isNoHitHandBall();	// 아무 공도 맞추지 못한 때,
@@ -16,9 +20,9 @@ public:
 	bool isEightBallBadToIn();	// 자신의 공을 다 넣지 않고, 8번 공 넣음
 	bool isEightBallWithFoul();	// 8번 공을 넣으면서 파울
 
-	bool isFoul();// 파울이면 프리볼을 선언
+	bool isFoul() const;// 파울이면 프리볼을 선언
 
-	bool isLose();//패배이면 패배라고 선언
+	bool isLose() const;//패배이면 패배라고 선언
 };
 
 #endif
