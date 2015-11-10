57ade73708eeb080d4ee0a7155ebce05c427532c
Donghwan Kim <wraithtorch@gmail.com>
2015-11-10 17:34:09 +0900

    add parameter in create()

--- CObject.h
+++ CObject.h
@@ -6,7 +6,7 @@ class CObject
 	float center_x, center_y, center_z;
 	D3DXMATRIX mLocal;
 public:
-	virtual bool create() = 0;
+	virtual bool create(IDirect3DDevice9* pDevice) = 0;
 	virtual void destroy() = 0;
 	virtual void draw(IDirect3DDevice9* pDevice, const D3DXMATRIX& mWorld) = 0;
 	D3DXVECTOR3 getPosition() const;
