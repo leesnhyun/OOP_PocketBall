0b74bf9640a7e1cf6563c9f1f7faf6ac37a7ee82
tjdgus3537 <tjdgus3537@naver.com>
2015-11-11 17:30:55 +0900

    CHole 생성자 --> default 생성자

--- CHole.h
+++ CHole.h
@@ -17,12 +17,12 @@ private:
 	// DirectX에서의 좌표는 화면에 대해서 앞으로 x축, 위로 y축, 오른쪽으로 z축이다.
 
 public:
-	CHole(float radius, D3DXCOLOR color = d3d::WHITE);
+	CHole();
 	~CHole();
 	bool create(IDirect3DDevice9* pDevice);									// 구멍을 화면에 생성함
 	void destroy();															// 구멍을 화면에서 소멸시킴
 	void draw(IDirect3DDevice9* pDevice, const D3DXMATRIX& mWorld);			// 구멍을 화면에 그려냄
-	bool hasIntersected(CSphere& ball);										// 구멍에 공이 들어가야 하는지 확인
+	bool hasIntersected(CSphere& ball) const;								// 구멍에 공이 들어가야 하는지 확인
 	void hitBy(CSphere& ball);												// 구멍을 넣어야 하는 경우 공을 제거하고 스택에 넣는다.
 	float getRadius() const;												// 구멍의 반지름을 받아옴
 	void setLocalTransform(const D3DXMATRIX& mLocal);						
